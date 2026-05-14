cd79b904112124a89a19f1c3f6b6ce7b70511a77
Lucimar02S <lucimarmay@gmail.com>
2026-05-14 20:47:49 -0300

    adicionando tupperware

---
+++ teste_vendas.py
@@ -0,0 +1,5 @@
+from vendas_tupperware import calcular_venda
+
+assert calcular_venda(10, 35) == 350
+
+print("Teste de vendas executado com sucesso")
---
+++ vendas_tupperware.py
@@ -0,0 +1,6 @@
+def calcular_venda(quantidade, valor_unitario):
+    return quantidade * valor_unitario
+
+total = calcular_venda(10, 35)
+
+print("Valor total da venda: R$", total)
